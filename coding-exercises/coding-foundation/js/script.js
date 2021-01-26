c826544a839e9afe10db11e745012bd5aa7b8945
alexis <qp274@nyu.edu>
2021-01-26 22:56:49 +0800

    comments

--- coding-exercises/coding-foundation/js/script.js
+++ coding-exercises/coding-foundation/js/script.js
@@ -15,19 +15,20 @@ function enter() {
 
 function add() {
   let n = document.getElementById('inbox').value;
-  let newresult = document.createElement('div');
+  let newresult = document.createElement('div'); //replace old content with new
   newresult.className = "wrapper";
   for (let i=1; i<=n; i+=1) {
     let newbox = document.createElement('div');
     newbox.className = "square";
-    newresult.appendChild(newbox);
-    randoms(newbox);
+    newresult.appendChild(newbox); //add each element into new content box
+    randoms(newbox); //randomize each element
     randomp(newbox,n);
     console.log(i);
   }
-  result.innerHTML=newresult.innerHTML;
+  result.innerHTML=newresult.innerHTML; //replace old content with new
 }
 
+// function for clearing the page
 function clear() {
   let blank = document.createElement('div');
   result.innerHTML = blank.innerHTML;
@@ -38,6 +39,7 @@ function clear() {
 //   console.log(value);
 // }
 
+// randomize dots positions
 function randomp(object,n) {
   let randomtop = Math.random() * window.innerHeight;
   let randomleft = Math.random() * window.innerWidth;
@@ -45,9 +47,11 @@ function randomp(object,n) {
   object.style.top = randomtop+'px';
   object.style.left = randomleft+'px';
   object.style.zIndex = randomzindex;
+  // to make the input box and buttons always on top
   user.style.zIndex = n+1;
 }
 
+// randomize dots size
 function randoms(object) {
   let randomsize = Math.random() * 100;
   object.style.width = randomsize +'px';
